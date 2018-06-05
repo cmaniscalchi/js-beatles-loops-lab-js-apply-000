216a36790d116c8ead48001651899d0c60757622
Claudia Maniscalchi <claudia.maniscalchi@gmail.com>
2018-06-05 00:22:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
+  
   for (let i = 0, i < 4; i++) {
     array.push(`${musicians[i]} plays ${instruments}`)
-    return array[][]
   }
+  return array;
 }
