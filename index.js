5816796cffd3be1aee94201c2fba4843e76f94cd
Claudia Maniscalchi <claudia.maniscalchi@gmail.com>
2018-06-05 00:18:27 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function theBeatlesPlay(musicians, instruments) {
+const musicians = [
+  "John Lennon",
+  "Paul McCartney",
+  "George Harrison",
+  "Ringo Starr"
+  ];
+  
+const instruments = [
+  "Guitar",
+  "Bass Guitar",
+  "Lead Guitar",
+  "Drums"
+  ];
+
+  var array = [];
+  for (let i = 0, i < musicians.length; i++) {
+    return `${musicians[i]} plays ${instruments}`
+  }
+}
